d5ed359e97dc97601c28c6fc86a0f5983cf1ed68
akelax-technology <akelaxtechnologie@gmail.com>
2021-06-30 17:57:44 +0100

    style: ajust font-size

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,21 @@
 let a = 2;
 let anime = 1
+
+
+$('.flag').forEach((element) => {
+    //element.classList.add('anime-' + a);
+    element.style = `animation: anime-flag-${anime} ${a}s infinite alternate 0s`
+    a++
+    anime++;
+    if (a > 5) {
+        a = 3
+    }
+    if (anime > 6) {
+        anime = 1;
+    }
+})
+a = 2;
+
 setInterval(() => {
     $('.flag').forEach((element) => {
         //element.classList.add('anime-' + a);
